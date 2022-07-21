0e74c2930f8a9f4cddd4095a07876691d043daa0
Alexis Hayat <alexis.hayat@cibiltech.com>
2022-07-21 22:59:24 +0200

    feat: add chmod for useful directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
   exit 1
 fi
 
-sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /var/www/$DOMAIN && rm -rf ..?* .[!.]* * && git clone $REPOSITORY_URL . && composer install && npm i && chmod 777 -R storage/ && chmod 777 -R public/"
+sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /var/www/$DOMAIN && rm -rf ..?* .[!.]* * && git clone $REPOSITORY_URL . && composer install && npm i && chmod 777 -R storage/ && chmod 777 -R public/ && service nginx restart"
 echo 'Done.'
