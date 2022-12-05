f4d8dba1b7e73466cd2568b4b55fbed3d31e7248
Alexis Hayat <alexis.hayat@gmail.com>
2022-12-05 02:25:46 +0100

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check if needed env vars are available
-if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN || -z $REPOSITORY_URL || -z $DOMAIN ]]; then
+if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN || -z $REPOSITORY_URL || -z $DOMAIN || -z $EMAIL ]]; then
   echo 'One or more needed env vars are undefined! Take a look at the example usage declaration.'
   exit 1
 fi
 
-echo 'cd /var/www/alexishayat.me && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart'
+sshpass -p "$SECRET_PASS" ssh git@github.com "cd /var/www/$DOMAIN && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
 
 echo 'Done.'
