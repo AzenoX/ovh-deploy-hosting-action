897536bf3afae5e018922f1d03881e1447d436f6
Alexis Hayat <alexis.hayat@gmail.com>
2022-12-05 01:33:23 +0100

    chore: update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,10 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
   exit 1
 fi
 
-sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /var/www/$DOMAIN && git pull . && composer install && npm i && service nginx restart"
+cd /var/www/alexishayat.me
+git reset --hard origin/master
+composer install
+npm i
+service nginx restart
+
 echo 'Done.'
