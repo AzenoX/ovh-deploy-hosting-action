339cd465e529bed40e895a26392d12507573c32c
Alexis Hayat <alexis.hayat@gmail.com>
2022-12-05 02:35:53 +0100

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
   exit 1
 fi
 
-echo $SECRET >> ~/.ssh/id_rsa
-sshpass -p "$SECRET_PASS" ssh -o StrictHostKeyChecking=no git@github.com "cd /var/www/$DOMAIN && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
+sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /var/www/$DOMAIN && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
 
 echo 'Done.'
