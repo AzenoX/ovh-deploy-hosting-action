2c8afaaf6f7b1a20da0bb77383dd4edf66ac2d8d
Alexis Hayat <alexis.hayat@gmail.com>
2022-12-06 22:49:25 +0100

    fix: entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN apk --update add --no-cache bash openssh-client sshpass \
   && rm -rf /var/cache/apk/*
 
 COPY entrypoint.sh /entrypoint.sh
-
+RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
 fi
 
 #sshpass -p "$OVH_HOSTING_PASSWORD" ssh $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /var/www/$DOMAIN && git fetch && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
-sshpass -p "$OVH_HOSTING_PASSWORD" ssh $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /root && touch test.txt"
+sshpass -p "$OVH_HOSTING_PASSWORD" -o StrictHostKeyChecking=no ssh $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "cd /root && touch test.txt"
 
 echo 'Done.'
