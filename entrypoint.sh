8e9a6a50c2d6b69a4784f81f57b387f108fcc630
Alexis Hayat <alexis.hayat@gmail.com>
2022-12-09 00:08:53 +0100

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
   exit 1
 fi
 
-sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "eval 'ssh-agent' && touch ~/.ssh/rsa_deploy && echo $SECRET >> ~/.ssh/rsa_deploy && touch ~/.ssh/rsa_deploy.pub && echo $SECRET_PUB >> ~/.ssh/rsa_deploy.pub && ssh-add ~/.ssh/rsa_deploy.pub && cd /var/www/$DOMAIN && ssh -o StrictHostKeyChecking=no -vT git@github.com && git fetch && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
+sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "eval 'ssh-agent -s' && touch ~/.ssh/rsa_deploy && echo $SECRET >> ~/.ssh/rsa_deploy && touch ~/.ssh/rsa_deploy.pub && echo $SECRET_PUB >> ~/.ssh/rsa_deploy.pub && ssh-add ~/.ssh/rsa_deploy.pub && cd /var/www/$DOMAIN && ssh -o StrictHostKeyChecking=no -vT git@github.com && git fetch && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
 
 echo 'Done.'
