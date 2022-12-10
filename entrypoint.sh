1d8f3257a1ca3aef75ddb57fd48907529566407f
Alexis Hayat <alexis.hayat@gmail.com>
2022-12-10 19:27:23 +0100

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ if [[ -z $OVH_HOSTING_USER || -z $OVH_HOSTING_PASSWORD || -z $OVH_HOSTING_DOMAIN
   exit 1
 fi
 
-sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "eval 'ssh-agent -s' && touch ~/.ssh/id_rsa && echo $SECRET >> ~/.ssh/id_rsa && touch ~/.ssh/id_rsa.pub && echo $SECRET_PUB >> ~/.ssh/id_rsa.pub && ssh-add && cd /var/www/$DOMAIN && ssh -o StrictHostKeyChecking=no -vT git@github.com && git fetch && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
+sshpass -p "$OVH_HOSTING_PASSWORD" ssh -o StrictHostKeyChecking=no $OVH_HOSTING_USER@$OVH_HOSTING_DOMAIN "eval 'ssh-agent -s' && ssh-add ~/.ssh/id_rsa_deploy_alexishayat_me && cd /var/www/$DOMAIN && ssh -o StrictHostKeyChecking=no -vT git@github.com && git fetch && git reset --hard origin/master && rm -rf vendor/* && composer install && composer dump-autoload && npm ci && service nginx restart"
 
 echo 'Done.'
